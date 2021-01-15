b1cbc7fe75eb18ef4610402654392ee0a3bf8c52
Michael I <mivanit@hotmail.com>
2021-01-15 17:30:12 -0500

    added tests

--- test_sample.py
+++ test_sample.py
@@ -1,31 +1,62 @@
+import unittest
+import dictmagic
+
+class TestDictmagic(unittest.TestCase):
+
+	def testing_basic(self):
+		
+		original = {
+			'a' : 1,
+			'b' : 2,
+			'c' : {
+				'x' : 42,
+				'y' : 'a string',
+				'z' : {
+					'asdf' : [1, 2, 3],
+					'qwerty' : 3.1415,
+				}
+			},
+		}
+
+		flat_expected = {
+			'a' : 1,
+			'b' : 2,
+			'c/x' : 42,
+			'c/y' : 'a string',
+			'c/z/asdf' : [1, 2, 3],
+			'c/z/qwert' : 3.1415,
+		}
+
+		flattened = dictmagic.flatten(original)
+		self.assertEqual(str(flattened), str(flat_expected))
+		should_be_same_as_original = dictmagic.unflatten(flattened)
+		self.assertEqual(str(original), str(should_be_same_as_original))
+		self.assertEqual(str(original), str(dictmagic.unflatten(flat_expected)))
 
 
-def testing_imports():
-	import dictmagic
+	def testing_duplication(self):
+		
+		for dup_key_repl in [None, 'None', '', '_', -1]:
+			dupeDict_flat = { 'a' : 'val1', 'a/b' : 'val2' }
 
-	original = {
-		'a' : 1,
-		'b' : 2,
-		'c' : {
-			'x' : 42,
-			'y' : 'a string',
-			'z' : {
-				'asdf' : [1, 2, 3],
-				'qwerty' : 3.1415,
-			}
-		},
-	}
+			dupeDict_unflat = { 'a' : { dup_key_repl : 'val1', 'b' : 'val2' } }
 
-	flattened = dictmagic.flatten(original)
+			test_unflat = dictmagic.unflatten(dupeDict_flat, dup_key_repl = dup_key_repl)
 
-	should_be_same_as_original = dictmagic.unflatten(flattened)
-	print('original:\t',original)
-	print('flattened:\t',flattened)
-	# print(should_be_same_as_original)
+			self.assertEqual(str(dupeDict_unflat), str(test_unflat))
+		
+	def testing_duplication_except(self):
+		
+		for dup_key_repl in [None, 'None', '', '_', -1]:
+			dupeDict_flat = { 'a' : 'val1', 'a/b' : 'val2' }
 
-	assert str(original) == str(should_be_same_as_original)
+			try:
+				test_unflat = dictmagic.unflatten(dupeDict_flat, dup_key_repl = dup_key_repl)
 
-	print('tests passed! but actually there arent any real tests yet! proceed with caution!')
+				self.assertTrue(False, 'did not except on duplicate!')
+			except KeyError:
+				pass
+
 
 if __name__ == "__main__":
-	testing_imports()
+	unittest.main()
