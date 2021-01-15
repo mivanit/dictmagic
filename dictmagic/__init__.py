d1b1c803b8292111d5400e526ab7625135a35bee
Michael I <mivanit@hotmail.com>
2021-01-15 18:14:58 -0500

    version 0.1.1 uploaded to PyPI

--- dictmagic/__init__.py
+++ dictmagic/__init__.py
@@ -1,4 +1,4 @@
-__version__ = "v0.1.0"
+__version__ = "v0.1.1"
 __description__ = "a package that makes it easier to do weird things with python dictionaries"
 
 from .obj import ObjectDict,dictObj,gen_instance_ctor
